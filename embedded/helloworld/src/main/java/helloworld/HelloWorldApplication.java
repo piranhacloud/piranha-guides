5b8d7ea8da3b06e1259336d426fcbf50960e2c3f
Manfred Riem <mriem@manorrock.com>
2023-12-05 16:23:28 -0600

    Fixes #65 - Update all guides to latest Piranha version (#66)

--- embedded/helloworld/src/main/java/helloworld/HelloWorldApplication.java
+++ embedded/helloworld/src/main/java/helloworld/HelloWorldApplication.java
@@ -3,7 +3,7 @@ package helloworld;
 import cloud.piranha.embedded.EmbeddedPiranha;
 import cloud.piranha.embedded.EmbeddedPiranhaBuilder;
 import cloud.piranha.http.impl.DefaultHttpServer;
-import cloud.piranha.http.webapp.HttpWebApplicationServerProcessor;
+import cloud.piranha.http.webapp.HttpWebApplicationServer;
 
 public class HelloWorldApplication {
 
@@ -13,8 +13,10 @@ public class HelloWorldApplication {
                 .servletMapping("HelloWorld", "/*")
                 .buildAndStart();
         
+        HttpWebApplicationServer webAppServer = new HttpWebApplicationServer();
+        webAppServer.addWebApplication(piranha.getWebApplication());
         DefaultHttpServer server = new DefaultHttpServer(8080, 
-                new HttpWebApplicationServerProcessor(piranha), false);
+                webAppServer, false);
         
         server.start();
     }
