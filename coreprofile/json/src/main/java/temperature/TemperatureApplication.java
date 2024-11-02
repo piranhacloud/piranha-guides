6f08bae40fb2a2b856c1f47fb831d3e4ab4f5ebf
Manfred Riem <mriem@manorrock.com>
2024-11-02 08:17:20 -0500

    Fixes #115 - Update Piranha Core Profile - Create a JSON REST service guide (#126)

--- coreprofile/json/src/main/java/temperature/TemperatureApplication.java
+++
@@ -1,8 +0,0 @@
-package temperature;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("")
-public class TemperatureApplication extends Application {
-}
