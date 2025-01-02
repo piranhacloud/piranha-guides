e701feb158cc95caae8693e82d538a9ef502c907
Manfred Riem <mriem@manorrock.com>
2025-01-02 16:29:32 -0600

    Fixes #127 - Move coreprofile/arquillian into main Piranha project

--- coreprofile/arquillian/src/main/java/arquillian/HelloArquillianApplication.java
+++
@@ -1,8 +0,0 @@
-package arquillian;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("")
-public class HelloArquillianApplication extends Application {
-}
